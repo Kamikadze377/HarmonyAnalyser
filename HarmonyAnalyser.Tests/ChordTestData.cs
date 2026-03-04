de9a148d7e63a9f245cd73f041cc2434c27cc7c6
Kamikadze377 <kamil.rudnik2@gmail.com>
2026-03-04 02:10:40 +0100

    Generalna restrukturyzacja repozytorium.

---
+++ HarmonyAnalyser.Tests/ChordTestData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonyAnalyser.Tests
+{
+    public static class ChordTestData
+    {
+        private static readonly List<string> Notes = new() { "C", "D", "E", "F", "G", "A", "B" };
+
+        public static ChordManager.Note N(
+            string step,
+            int octave = 4,
+            int alter = 0,
+            int measure = 1,
+            int point = 0)
+        {
+            return new ChordManager.Note
+            {
+                Step = step,
+                Octave = octave,
+                Alter = alter,
+                MeasureNumber = measure,
+                Point = point
+            };
+        }
+
+        public static IEnumerable<object[]> Chords1() => Chords(1);
+        public static IEnumerable<object[]> Chords2() => Chords(2);
+        public static IEnumerable<object[]> Chords3() => Chords(3);
+        public static IEnumerable<object[]> Chords4() => Chords(4);
+        public static IEnumerable<object[]> Chords5() => Chords(5);
+
+        private static IEnumerable<object[]> Chords(int numberOfNotes)
+        {
+            foreach (var chord in PermutationHelper.GetPermutations(Notes, numberOfNotes))
+                yield return new object[] { chord };
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 HarmonyAnalyser
 
-Przed uruchomieniem programu nalezy zainstalowac czcionke Bravura (folder "Font").
+Przed uruchomieniem programu nalezy zainstalowac czcionke Bravura (folder "Font"). 
 Przykladowe pliki do analizy znajduja sie w folderze "Sample Songs".
 
-Before running the program, install the Bravura font (located in the "Font" folder).
+Before running the program, install the Bravura font (located in the "Font" folder). 
 Sample files for analysis can be found in the "Sample Songs" folder.
